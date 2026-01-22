b83bc4585cc86e1ec502dbe17acc3f03da3ad08f
GiRi <alexge92@gmail.com>
2026-01-22 12:03:07 +0200

    Created TimeStampedModel, solves #4

---
+++ open_ithageneia/managers.py
@@ -0,0 +1,12 @@
+from django.db.models.query import QuerySet
+from django.utils import timezone
+
+
+class TimeStampedQueryset(QuerySet):
+
+    def update(self, **kwargs):
+        # Also runs for bulk_update (https://docs.djangoproject.com/en/6.0/ref/models/querysets/#bulk-update)
+
+        kwargs["date_updated"] = timezone.now()
+
+        return super().update(**kwargs)
