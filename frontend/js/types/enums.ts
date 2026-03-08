5e955a51cbbeb7f7222799d503a7220269690ac3
Ilias Kontonis <62603210+ilias6@users.noreply.github.com>
2026-03-08 12:54:42 +0200

    wip

---
+++ frontend/js/types/enums.ts
@@ -0,0 +1,11 @@
+﻿export enum QuizCategory {
+	GEOGRAPHY = 'GEOGRAPHY',
+	CIVICS = 'CIVICS',
+	HISTORY = 'HISTORY',
+	CULTURE = 'CULTURE',
+}
+
+export enum StatementType {
+	TRUE_FALSE = 'TRUE_FALSE',
+	MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
+}
--- quiz/views.py
+++ quiz/views.py
@@ -4,4 +4,9 @@ from quiz.services import QuizService
 
 
 def random_quiz_view(request):
-	return render(request, 'Exam', props=QuizService.random_quiz(request.GET.dict()))
+	return render(
+		request,
+		'Exam',
+		props={
+			"exam": QuizService.random_quiz(request.GET.dict())
+		})
