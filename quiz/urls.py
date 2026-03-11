c5943025f12f5a843f26e28dda801ebff620ef35
GiRi <alexge92@gmail.com>
2026-03-11 15:27:56 +0200

    Training (#44)
    * Training page logic init approach
    
    * More efficient query via raw sql

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+
 from . import views
 
 router = DefaultRouter()
 
 urlpatterns = [
     path("", views.random_quiz_view, name="random-quiz"),
+    path("training", views.training, name="training"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,28 @@
+from django.http import JsonResponse
 from inertia import render
 
 from quiz.services import QuizService
+
+from .serializers import ExerciseQuerySerializer
 
 
 def random_quiz_view(request):
     return render(
         request, "Exam", props={"exam": QuizService.random_quiz(request.GET.dict())}
     )
+
+
+def training(request):
+    query_serializer = ExerciseQuerySerializer(data=request.GET)
+
+    if not query_serializer.is_valid():
+        return JsonResponse({"errors": query_serializer.errors}, status=400)
+
+    validated_data = query_serializer.validated_data
+
+    data_by_category = QuizService.get_by_category(
+        category=validated_data["category"],
+        amount=validated_data["amount"],
+    )
+
+    return render(request, "Training", props={"data": data_by_category})
